0983fb02f8083bb30075860ae927af5cc66d8a99
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-12 14:56:16 +0300

    Test AsOrdered

--- AsParallelLinq/Program.cs
+++ AsParallelLinq/Program.cs
@@ -13,14 +13,14 @@ namespace AsParallelLinq
             SeparateEvent separateEvent = new SeparateEvent();
             separateEvent.OnSeparate += consoleSeparator.Separator;
             int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8  };
-            (from n in numbers.AsParallel()
-             select Factorial(n)).ForAll(Console.WriteLine);
+            var factorials = from n in numbers.AsParallel().AsOrdered()
+                             select Factorial(n);
             separateEvent.Display();
-            //foreach (var x in factorials)
-            //{
-            //    Console.WriteLine(x);
+            foreach (var x in factorials)
+            {
+                Console.WriteLine(x);
                 
-            //}
+            }
         }
 
         private static int Factorial(int n)
